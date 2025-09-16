0ce9d8018522d62e88a55f140147e1b894336149
wzq448744 <wzq448744@alibaba-inc.com>
2025-09-16 19:19:54 +0800

    Sync local config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.softtabstop = 4
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+
+vim.g.autoformat = false
